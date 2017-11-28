469dd6524efb26c635f0c0df9d82c744e4e396c9
Matt Silbernagel <silbermm@gmail.com>
2017-11-27 20:52:26 -0500

    add port

--- config/prod.exs
+++ config/prod.exs
@@ -2,6 +2,7 @@ use Mix.Config
 
 config :frequency, FrequencyWeb.Endpoint,
   load_from_system_env: true,
+  http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "public-frequency.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
