619c6cebba42ad17ebf737f91c20e61457453f5a
Matt Silbernagel <silbermm@gmail.com>
2017-11-27 19:40:33 -0500

    add url

--- config/prod.exs
+++ config/prod.exs
@@ -2,14 +2,14 @@ use Mix.Config
 
 config :frequency, FrequencyWeb.Endpoint,
   on_init: {FrequencyWeb.Endpoint, :load_from_system_env, []},
-  url: [host: "example.com", port: 4000],
+  url: [host: "frequency.gigalixirapp.com", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   secret_key_base: "${SECRET_KEY_BASE}"
 
 config :frequency, Frequency.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: "${DATABASE_URL}",
+  url: {:system, "DATABASE_URL"},
   database: "",
   pool_size: 1,
   ssl: true
