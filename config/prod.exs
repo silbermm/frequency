5427e92e810b3ae64509bd63bb6ddb7476e73d8b
Matt Silbernagel <silbermm@gmail.com>
2017-11-27 20:00:52 -0500

    set up for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -1,17 +1,16 @@
 use Mix.Config
 
 config :frequency, FrequencyWeb.Endpoint,
-  on_init: {FrequencyWeb.Endpoint, :load_from_system_env, []},
-  url: [host: "frequency.gigalixirapp.com", port: 4000],
+  load_from_system_env: true,
+  url: [scheme: "https", host: "public-frequency.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  server: true,
-  secret_key_base: "${SECRET_KEY_BASE}"
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 config :frequency, Frequency.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: {:system, "DATABASE_URL"},
-  database: "",
-  pool_size: 1,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
 config :logger, level: :info
